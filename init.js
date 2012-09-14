b0fc2a6bea78b71556953e6dd2df6886f6bc34ce
Pete Schaffner <pjschaffner@gmail.com>
2012-09-14 13:24:04 -0400

    First implementation working
    NOTE: Open files inside "Application Support" won't open

---
+++ init.js
@@ -0,0 +1,19 @@
+/*!
+ * Open Terminal Here mixin for Chocolat
+ * WTFPL 2 Licensed
+ */
+
+var exec = require('child_process').exec;
+
+Hooks.addMenuItem('Go/Open Terminal Here', 'cmd+shift+o', function () {
+  if (!Document.current() || !Document.current().path()) {
+    Alert.show('Whoops!', 'A file must be open for this to work.', ['Dammit']);
+    return;
+  }
+
+  var filePath = Document.current().path(),
+      fileName = Document.current().displayName(),
+      dirPath = filePath.replace(fileName, '');
+
+  exec('open -a Terminal ' + dirPath);
+});
